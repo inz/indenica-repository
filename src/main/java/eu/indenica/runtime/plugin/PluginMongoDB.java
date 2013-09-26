786360051b9637633361238f7c59d49c74635ac2
Christian Inzinger <inzinger@dsg.tuwien.ac.at>
2013-09-26 14:52:42 +0200

    repo. minor update.

--- src/main/java/eu/indenica/runtime/plugin/PluginMongoDB.java
+++ src/main/java/eu/indenica/runtime/plugin/PluginMongoDB.java
@@ -4,6 +4,8 @@ import java.net.UnknownHostException;
 
 import net.sf.json.JSON;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 
 import at.ac.tuwien.infosys.util.Util;
@@ -21,82 +23,87 @@ import eu.indenica.runtime.dto.Data;
 import eu.indenica.runtime.dto.Filter;
 
 public class PluginMongoDB implements IPlugin {
+    private static final Logger LOG = LoggerFactory
+            .getLogger(PluginMongoDB.class);
 
-	public static Util util = new Util();
-	private DB db;
-	private DBAddress address;
-	private String dbName = "indenica";
-	private String collectionName = "indenica";
-	private DBCollection collection;
+    public static Util util = new Util();
+    private DB db;
+    private DBAddress address;
+    private String dbName = "indenica";
+    private String collectionName = "indenica";
+    private DBCollection collection;
 
-	static {
-		System.out.println("Starting embedded MongoDB instance");
-		MongoDBRuntime.getDefaultInstance();
-	}
+    static {
+        LOG.info("Starting embedded MongoDB instance");
+        MongoDBRuntime.getDefaultInstance();
+    }
 
-	public PluginMongoDB(String host) {
-		try {
-			address = new DBAddress(host, dbName);
-		} catch (UnknownHostException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public PluginMongoDB(String host) {
+        try {
+            address = new DBAddress(host, dbName);
+        } catch(UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public Data storeData(Data d) {
-		System.out.println("Storing data in MongoDB");
-		DBCollection coll = getCollection(collectionName);
-		JSON json = util.xml.toJSON((Element)d.value);
-		coll.save((DBObject) com.mongodb.util.JSON.parse(json.toString(0)));
-		return d;
-	}
+    public Data storeData(Data d) {
+        LOG.debug("Storing data in MongoDB");
+        DBCollection coll = getCollection(collectionName);
+        JSON json = util.xml.toJSON((Element) d.value);
+        coll.save((DBObject) com.mongodb.util.JSON.parse(json.toString(0)));
+        return d;
+    }
 
-	public Data getData(Filter f) {
-		try {
-			DBCollection coll = getCollection(collectionName);
+    public Data getData(Filter f) {
+        try {
+            DBCollection coll = getCollection(collectionName);
 
-			String query = util.xml.getChildElements(((Element)f.value), "query").get(0).getTextContent();
-			System.out.println("Finding data in MongoDB: " + query);
-			DBObject search = (DBObject)com.mongodb.util.JSON.parse(query);
+            String query =
+                    util.xml.getChildElements(((Element) f.value), "query")
+                            .get(0).getTextContent();
+            LOG.debug("Finding data in MongoDB: " + query);
+            DBObject search = (DBObject) com.mongodb.util.JSON.parse(query);
 
-			Data d = new Data();
-			d.value = util.xml.toElement("<mongodb/>");
-			DBCursor c = coll.find(search);
-			while(c.hasNext()) {
-				DBObject o = c.next();
-				util.xml.appendChild((Element)d.value, util.xml.toElement(
-						com.mongodb.util.JSON.serialize(o)));
-			}
+            Data d = new Data();
+            d.value = util.xml.toElement("<mongodb/>");
+            DBCursor c = coll.find(search);
+            while(c.hasNext()) {
+                DBObject o = c.next();
+                util.xml.appendChild((Element) d.value,
+                        util.xml.toElement(com.mongodb.util.JSON.serialize(o)));
+            }
 
-			return d;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+            return d;
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private DBCollection getCollection(String collection2) {
-		if(collection == null) {
-			collection =getDB().getCollection(collectionName);
-		}
-		return collection;
-	}
+    private DBCollection getCollection(String collection2) {
+        if(collection == null) {
+            collection = getDB().getCollection(collectionName);
+        }
+        return collection;
+    }
 
-	public boolean canHandle(Data d) {
-		return (d.value instanceof Element) && 
-				((Element)d.value).getNodeName().equals(
-						Constants.DATA_MONGODB);
-	}
-	public boolean canHandle(Filter f) {
-		return (f.value instanceof Element) && 
-				((Element)f.value).getNodeName().equals(
-						Constants.DATA_MONGODB);
-	}
+    public boolean canHandle(Data d) {
+        return (d.value instanceof Element)
+                && ((Element) d.value).getNodeName().equals(
+                        Constants.DATA_MONGODB);
+    }
 
-	/* HELPER METHODS */
+    public boolean canHandle(Filter f) {
+        return (f.value instanceof Element)
+                && ((Element) f.value).getNodeName().equals(
+                        Constants.DATA_MONGODB);
+    }
 
-	private DB getDB() {
-		if(db == null) {
-			db = Mongo.connect(address);
-		}
-		return db;
-	}
+    /* HELPER METHODS */
+
+    private DB getDB() {
+        if(db == null) {
+            db = Mongo.connect(address);
+        }
+        return db;
+    }
 }
